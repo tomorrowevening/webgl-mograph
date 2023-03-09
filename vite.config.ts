a7b9b3fd676955bbcc1bd65c552a67d3f220f48b
Colin Duffy <colin@tomorrowevening.com>
2023-03-08 21:45:22 -0800

    fix(glsl): glslify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import glsl from 'vite-plugin-glsl'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [glsl(), react()],
 })

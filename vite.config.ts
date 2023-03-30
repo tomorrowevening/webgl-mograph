bc20019d1fd65be05935ee6e01127588679187ec
Colin Duffy <colin@tomorrowevening.com>
2023-03-30 13:01:54 -0700

    fix(css): image paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.png'],
   plugins: [glsl(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+      "~@": path.resolve(__dirname, "/src"),
+    }
+  }
 })

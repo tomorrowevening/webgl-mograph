ab0eb6671b9d1b0128aad9b26dbd57c34a5a3ad8
Colin Duffy <colin@tomorrowevening.com>
2023-03-31 16:39:43 -0700

    feat(welcome): scss

--- src/components/main/welcome/index.tsx
+++ src/components/main/welcome/index.tsx
@@ -1,9 +1,11 @@
 // Models
 import { Events, threeDispatcher } from '../../../models/constants'
+// Components
+import './welcome.scss'
 
 export default function Welcome() {
   return (
-    <div className="absoluteCenter">
+    <div className="welcome absoluteCenter">
       <h1>Welcome</h1>
       <p>Description</p>
       <button

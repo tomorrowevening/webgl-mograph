cf25703d56af127a996e72826619a94d9e9586de
Colin Duffy <colin@tomorrowevening.com>
2023-04-04 13:46:53 -0700

    fix(ux): oninit return mesh

--- src/components/ux/MeshButton.tsx
+++ src/components/ux/MeshButton.tsx
@@ -3,8 +3,9 @@ import { CSSProperties, useEffect } from 'react'
 import { Vector2 } from 'three'
 // Models
 import type { MeshUX } from '@/types'
-// Components
+// Views
 import './button.scss'
+// Controllers
 import scenes from '@/controllers/SceneController'
 
 export default function MeshButton(props: MeshUX) {
@@ -18,15 +19,19 @@ export default function MeshButton(props: MeshUX) {
   if (props.bottom !== undefined) style.bottom = `${props.bottom}px`
 
   useEffect(() => {
-    scenes.addMesh(
+    const mesh = scenes.addMesh(
       props.name,
       props.width,
       props.height,
       null,
       props.align,
-      new Vector2(props?.left, props?.top),
+      new Vector2(
+        props?.left !== undefined ? props?.left : props?.right,
+        props?.top !== undefined ? props?.top : props?.bottom,
+      ),
       props?.material,
     )
+    if (props.onInit !== undefined) props.onInit(mesh)
   }, [])
 
   return (

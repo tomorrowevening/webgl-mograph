9281a8a4b82ccd0d0971f50460eab4f6a7d74fb0
Colin Duffy <colin@tomorrowevening.com>
2023-04-07 16:58:49 -0700

    fix(intro): cleaned up

--- src/scenes/intro/index.ts
+++ src/scenes/intro/index.ts
@@ -1,43 +1,22 @@
 // Libs
 import {
   AmbientLight,
-  Clock,
   HalfFloatType,
-  Mesh,
   OrthographicCamera,
   PerspectiveCamera,
   PointLight,
-  Shape,
-  TorusGeometry,
-  TorusKnotGeometry,
-  Vector2,
-  Vector3,
   WebGLRenderTarget,
 } from 'three'
-import { SVGLoader, SVGResult, SVGResultPaths } from 'three/examples/jsm/loaders/SVGLoader'
 import gsap from 'gsap'
 import { EffectComposer, EffectPass, FXAAEffect, Pass, RenderPass, VignetteEffect } from 'postprocessing'
 // Models
-import assets from '@/models/assets'
-import { IS_DEV } from '@/models/constants'
 import webgl from '@/models/webgl'
 // Views
 import BaseScene from '../BaseScene'
-import LineGeometry from '@/geometry/LineGeometry'
-import StrokeMaterial from '@/materials/StrokeMaterial'
-import TPMeshBasicMaterial from '@/materials/textureProjection/TPMeshBasicMaterial'
-import TPMeshPhysicalMaterial from '@/materials/textureProjection/TPMeshPhysicalMaterial'
-// Utils
-import { debugColor, debugFolder, debugInput } from '@/utils/debug'
 
 export default class IntroScene extends BaseScene {
   composer!: EffectComposer
   private mainCamera: PerspectiveCamera
-
-  unlitTP!: TPMeshBasicMaterial
-  torusKnot!: Mesh
-  litTP!: TPMeshPhysicalMaterial
-  torus!: Mesh
 
   constructor() {
     super('intro')
@@ -59,95 +38,6 @@ export default class IntroScene extends BaseScene {
 
   protected override initMesh(): Promise<void> {
     return new Promise((resolve) => {
-      const texture = assets.textures.get('uv_grid')
-      const target = new Vector3(0, 0, 100)
-      this.camera.updateProjectionMatrix()
-
-      this.unlitTP = new TPMeshBasicMaterial({
-        projectedMap: texture.clone(),
-        targetPos: target,
-        camWorldInverse: this.camera.matrixWorldInverse.clone(),
-        camProjection: this.camera.projectionMatrix.clone(),
-      })
-      this.torusKnot = new Mesh(new TorusKnotGeometry(100, 20, 18, 36), this.unlitTP)
-      this.torusKnot.name = 'torusKnotMesh'
-      this.torusKnot.position.x = 250
-      this.world.add(this.torusKnot)
-
-      this.litTP = new TPMeshPhysicalMaterial({
-        projectedMap: texture.clone(),
-        targetPos: target,
-        camWorldInverse: this.camera.matrixWorldInverse.clone(),
-        camProjection: this.camera.projectionMatrix.clone(),
-        metalness: 0.5,
-        roughness: 0.5,
-      })
-
-      this.torus = new Mesh(new TorusGeometry(100, 20, 18, 36), this.litTP)
-      this.torus.name = 'torusMesh'
-      this.torus.position.x = -250
-      this.world.add(this.torus)
-
-      const loader = new SVGLoader()
-      loader.load('svg/test.svg', (data: SVGResult) => {
-        data.paths.forEach((path: SVGResultPaths) => {
-          const shapes = SVGLoader.createShapes(path)
-          shapes.forEach((shape: Shape) => {
-            const vectors = shape.getPoints()
-            const pts: Array<number[]> = []
-            vectors.forEach((v: Vector2) => {
-              pts.push([v.x, -v.y])
-            })
-            const lineGeom = new LineGeometry(pts, { closed: false, distances: true })
-            const lineMat = new StrokeMaterial({
-              diffuse: 0xffffff,
-              thickness: 1,
-            })
-            const line = new Mesh(lineGeom, lineMat)
-            line.name = 'line'
-            line.position.set(-100, 100, 0)
-            this.world.add(line)
-
-            if (IS_DEV) {
-              const folder = debugFolder('Line')
-              debugInput(folder, lineMat, 'alpha', {
-                min: 0,
-                max: 1,
-              })
-              debugColor(folder, lineMat, 'diffuse')
-              debugInput(folder, lineMat, 'thickness', {
-                min: 0,
-                max: 20,
-              })
-              debugInput(folder, lineMat, 'dash', {
-                min: 0,
-                max: 10,
-              })
-              debugInput(folder, lineMat, 'dashGap', {
-                min: 0,
-                max: 10,
-              })
-              debugInput(folder, lineMat, 'dashOffset', {
-                min: 0,
-                max: 10,
-              })
-              debugInput(folder, lineMat, 'trimStart', {
-                min: -1,
-                max: 1,
-              })
-              debugInput(folder, lineMat, 'trimEnd', {
-                min: -1,
-                max: 1,
-              })
-              debugInput(folder, lineMat, 'trimOffset', {
-                min: -1,
-                max: 1,
-              })
-            }
-          })
-        })
-      })
-
       resolve()
     })
   }
@@ -183,26 +73,7 @@ export default class IntroScene extends BaseScene {
   }
 
   override update(): void {
-    const time = this.clock.getElapsedTime()
-
-    this.torusKnot.rotateX(0.01)
-    this.torusKnot.rotateY(0.02)
-    this.torusKnot.rotateZ(0.005)
-
-    this.torus.rotateX(-0.005)
-    this.torus.rotateY(-0.01)
-    this.torus.rotateZ(-0.02)
-
-    const mainCam = this.mainCamera
-    mainCam.updateProjectionMatrix()
-    mainCam.updateMatrixWorld()
-
-    this.unlitTP.camWorldInverse.copy(mainCam.matrixWorldInverse)
-    this.unlitTP.camProjection.copy(mainCam.projectionMatrix)
-
-    this.litTP.camWorldInverse.copy(mainCam.matrixWorldInverse)
-    this.litTP.camProjection.copy(mainCam.projectionMatrix)
-    this.litTP.blendAmt = Math.sin(time) * 0.5 + 0.5
+    //
   }
 
   override draw(renderTarget: WebGLRenderTarget | null): void {

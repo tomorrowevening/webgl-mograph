49f33f2e7e701274626428eef08a9ce3e360581a
Colin Duffy <colin@tomorrowevening.com>
2023-04-26 11:59:54 -0700

    feat(post): blending list

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import { Material, Vector2 } from 'three'
+import { BlendFunction } from 'postprocessing'
 import TextMesh from './mesh/ui/TextMesh'
 import UIMesh from './mesh/ui/UIMesh'
 
@@ -36,3 +37,138 @@ export interface MeshUX {
   onRollOut?: () => void
   onClick?: () => void
 }
+
+export const BlendOptions = [
+  {
+    text: 'ADD',
+    value: BlendFunction.ADD,
+  },
+  {
+    text: 'ALPHA',
+    value: BlendFunction.ALPHA,
+  },
+  {
+    text: 'AVERAGE',
+    value: BlendFunction.AVERAGE,
+  },
+  {
+    text: 'COLOR',
+    value: BlendFunction.COLOR,
+  },
+  {
+    text: 'COLOR_BURN',
+    value: BlendFunction.COLOR_BURN,
+  },
+  {
+    text: 'COLOR_DODGE',
+    value: BlendFunction.COLOR_DODGE,
+  },
+  {
+    text: 'DARKEN',
+    value: BlendFunction.DARKEN,
+  },
+  {
+    text: 'DIFFERENCE',
+    value: BlendFunction.DIFFERENCE,
+  },
+  {
+    text: 'DIVIDE',
+    value: BlendFunction.DIVIDE,
+  },
+  {
+    text: 'DST',
+    value: BlendFunction.DST,
+  },
+  {
+    text: 'EXCLUSION',
+    value: BlendFunction.EXCLUSION,
+  },
+  {
+    text: 'HARD_LIGHT',
+    value: BlendFunction.HARD_LIGHT,
+  },
+  {
+    text: 'HARD_MIX',
+    value: BlendFunction.HARD_MIX,
+  },
+  {
+    text: 'HUE',
+    value: BlendFunction.HUE,
+  },
+  {
+    text: 'INVERT',
+    value: BlendFunction.INVERT,
+  },
+  {
+    text: 'INVERT_RGB',
+    value: BlendFunction.INVERT_RGB,
+  },
+  {
+    text: 'LIGHTEN',
+    value: BlendFunction.LIGHTEN,
+  },
+  {
+    text: 'LINEAR_BURN',
+    value: BlendFunction.LINEAR_BURN,
+  },
+  {
+    text: 'LINEAR_DODGE',
+    value: BlendFunction.LINEAR_DODGE,
+  },
+  {
+    text: 'LINEAR_LIGHT',
+    value: BlendFunction.LINEAR_LIGHT,
+  },
+  {
+    text: 'LUMINOSITY',
+    value: BlendFunction.LUMINOSITY,
+  },
+  {
+    text: 'MULTIPLY',
+    value: BlendFunction.MULTIPLY,
+  },
+  {
+    text: 'NEGATION',
+    value: BlendFunction.NEGATION,
+  },
+  {
+    text: 'NORMAL',
+    value: BlendFunction.NORMAL,
+  },
+  {
+    text: 'OVERLAY',
+    value: BlendFunction.OVERLAY,
+  },
+  {
+    text: 'PIN_LIGHT',
+    value: BlendFunction.PIN_LIGHT,
+  },
+  {
+    text: 'REFLECT',
+    value: BlendFunction.REFLECT,
+  },
+  {
+    text: 'SATURATION',
+    value: BlendFunction.SATURATION,
+  },
+  {
+    text: 'SCREEN',
+    value: BlendFunction.SCREEN,
+  },
+  {
+    text: 'SOFT_LIGHT',
+    value: BlendFunction.SOFT_LIGHT,
+  },
+  {
+    text: 'SRC',
+    value: BlendFunction.SRC,
+  },
+  {
+    text: 'SUBTRACT',
+    value: BlendFunction.SUBTRACT,
+  },
+  {
+    text: 'VIVID_LIGHT',
+    value: BlendFunction.VIVID_LIGHT,
+  },
+]
